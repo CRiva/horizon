1cee7885dfd3d3c4e10e86e564d9daf3918dd337
HParker <ahess@westmont.edu>
2013-09-24 18:47:47 -0700

    name uniqueness is checked

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_and_belongs_to_many :pages
-  # validates :name, uniqueness: true # make sure name is unique
+  validates :name, uniqueness: true # make sure name is unique
   before_create :setup_role
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
