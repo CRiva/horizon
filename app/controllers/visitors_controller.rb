f6c545b109f22824a8891b585a7258525a55ef4d
Avir94 <CRiva94@gmail.com>
2013-12-03 22:59:46 -0800

    adam's front page picture changes

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController < ApplicationController
 
   def new
     # just load articles for the home page.
-    @slides = Article.where("photo_file_name !=?", "/photos/original/missing.png").reverse.take(3)
+    @slides = Article.published.where("photo_file_name !=?", "/photos/original/missing.png").reverse.take(3)
     @articles = Article.published.order('created_at DESC').page(params[:page]).per(15)
   end
 
