5520225bae0359badc0d85684412a13237e4cdb2
Avir94 <CRiva94@gmail.com>
2014-09-18 11:04:45 -0700

    it works now... sorry alex for pushing broken code, I wont do it again

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 RailsBootstrap::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.westmont.edu",
+  :port => 25
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ RailsBootstrap::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'horizon.westmont.edu' }
+  config.action_mailer.default_url_options = { host: 'smtp.westmont.edu' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.westmont.edu",
