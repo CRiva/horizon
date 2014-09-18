d00a7883d72e22031b03f7bcc33182e4dbc1ce60
Avir94 <CRiva94@gmail.com>
2014-09-18 10:30:36 -0700

    still testing production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,12 @@ RailsBootstrap::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'smtp.westmont.edu' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.westmont.edu",
+    :port                 => 587,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
