9ef0d9857e3e1e17a6c002d97d413c4271a93261
CRiva94 <CRiva94@gmail.com>
2015-11-15 19:43:29 -0800

    added fix to master

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
                     },
                     default_url: "blank_profile.png"
 
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  
   validates :name, uniqueness: true
 
   before_create :setup_role
